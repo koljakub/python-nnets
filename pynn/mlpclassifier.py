91df62994f59315f28c5ab04b855f47dbb3b3096
Jakub Kol <jakub.kol@hotmail.com>
2018-03-26 01:37:01 +0200

    Add backpropagate method to the MLP
    Add an initial implementation of the
    Backpropagation algorithm to the MLP.
    
    Change parameter storing from the dictionary
    of parameters to lists of weights and biases.

--- pynn/mlpclassifier.py
+++ pynn/mlpclassifier.py
@@ -1,5 +1,6 @@
 import numpy as np
 from pynn.activations import *
+from collections import deque
 
 
 class MLPClassifier:
@@ -7,7 +8,8 @@ class MLPClassifier:
     def __init__(self, layers):
         self.layers = layers
         self.num_layers = len(layers)
-        self.parameters = {}
+        self.weights = []
+        self.biases = []
         self._initialize_parameters()
 
     def _initialize_parameters(self):
@@ -17,8 +19,8 @@ class MLPClassifier:
         :return:
         """
         for i in range(1, self.num_layers):
-            self.parameters["W" + str(i)] = np.random.randn(self.layers[i], self.layers[i-1]) * np.sqrt(2.0 / self.layers[i-1])
-            self.parameters["b" + str(i)] = np.zeros((self.layers[i], 1))
+            self.weights.append(np.random.randn(self.layers[i], self.layers[i - 1]) * np.sqrt(2.0 / self.layers[i - 1]))
+            self.biases.append(np.zeros((self.layers[i], 1)))
 
     def _feedforward(self, X):
         """
@@ -29,10 +31,41 @@ class MLPClassifier:
         """
         activations = []
         A = X
-        for i in range(1, self.num_layers - 1):
-            Z = self.parameters["W" + str(i)].dot(A) + self.parameters["b" + str(i)]
+        activations.append(A)
+
+        for i in range(0, len(self.weights) - 1):
+            Z = self.weights[i].dot(A) + self.biases[i]
             A = relu(Z)
             activations.append(A)
-        Z = self.parameters["W" + str(i+1)].dot(A) + self.parameters["b" + str(i+1)]
+        Z = self.weights[-1].dot(A) + self.biases[-1]
         activations.append(softmax(Z))
         return activations
+
+    def _backpropagate(self, X, Y):
+        """
+        Backpropagation alg.
+        :return:
+        """
+        batch_size = X.shape[1]
+        grad_w = deque()
+        grad_b = deque()
+
+        activations = self._feedforward(X)
+        delta = activations[-1] - Y
+        grad_w.append((1.0 / batch_size) * np.outer(delta, activations[-2].T))
+        grad_b.append((1.0 / batch_size) * np.sum(delta, axis = 1, keepdims = True))
+        for i in range(self.num_layers - 2, 0, -1):
+            delta = self.weights[i].T.dot(delta)
+            grad_w.appendleft((1.0 / batch_size) * np.outer(delta, activations[i - 1].T))
+            grad_b.appendleft((1.0 / batch_size) * np.sum(delta, axis=1, keepdims=True))
+        return (grad_w, grad_b)
+
+    def predict(self, X):
+        return np.argmax(self._feedforward(X.T)[-1], axis = 0)
+
+# if __name__ == '__main__':
+#     net = MLPClassifier([50, 5, 4, 3])
+#     X = np.random.randn(1, 50)
+#     Y = np.array([1, 0, 0]).reshape(3, 1)
+#     gradient = net._backpropagate(X.T, Y)
+#     print(gradient)
