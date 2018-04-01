28963ffe90c142c57c5fb6d5ffd7d2ac34b2e27e
Jakub Kol <jakub.kol@hotmail.com>
2018-04-01 14:43:23 +0200

    Improve the "layers" module
    Add the following  new functionality and improvements to the module:
    - Refactored base Layer class, new abstract methods for forward and
      backward steps were added in order to ensure that every future layer
      will override these key methods. Functionality for layer connecting
      was slightly improved.
    - Forward method in Dense layer class which represents a single step
      during the feedforward pass through the network.
    - Backward method in Dense layer class which represents a single step
      during the backward pass through the network.
    - New type of layer - Output - has been added to the module.

--- pynn/layers.py
+++ pynn/layers.py
@@ -1,3 +1,4 @@
+import abc
 from pynn.initializers import *
 from pynn.activations import *
 
@@ -6,46 +7,79 @@ class Layer:
 
     def __init__(self, n_units, activation_f):
         self.n_units = n_units
-        self.activation_f = activation_f
+        self.activation_f = ActivationFunction(activation_f)
+        self.d_activation_f = ActivationFunctionDerivative(activation_f)
         self.prev_layer = None
+        self.next_layer = None
+        self.A = None
+        self.W = None
+        self.b = None
+        self.d_A = None
+        self.d_W = None
+        self.d_b = None
+        self.delta = None
 
-    def connect_layer(self, layer):
-        self.prev_layer = layer
+    def connect_previous_layer(self, prev_layer):
+        self.prev_layer = prev_layer
+
+    def connect_next_layer(self, next_layer):
+        self.next_layer = next_layer
+
+    @abc.abstractmethod
+    def forward(self, A_prev):
+        return
+
+    @abc.abstractmethod
+    def backward(self):
+        return
 
     def __str__(self):
-        return self.__class__.__name__ + " layer " + str(self.n_units) + "units"
+        return self.__class__.__name__ + " layer ({}) | {} units".format(self.activation_f.name, self.n_units)
 
 
 class Input(Layer):
 
     def __init__(self, n_units):
-        Layer.__init__(self, n_units, identity)
+        Layer.__init__(self, n_units, "identity")
 
-    def foo(self):
-        print(self.n_units)
+    def forward(self, X):
+        self.A = self.activation_f(X)
+
+    def backward(self):
+        raise NotImplementedError("Input layer does not have any parameters which can be updated via backward step.")
 
 
 class Dense(Layer):
 
-    def __init__(self, n_units, initializer=HeInitializer(42), activation_f=relu):
+    def __init__(self, n_units, initializer=HeInitializer(42), activation_f="relu"):
         Layer.__init__(self, n_units, activation_f)
         self.initializer = initializer
-        self.A = None
 
     def initialize_parameters(self, prev_n_units):
         self.W, self.b = self.initializer.initialize((self.n_units, prev_n_units))
 
-    def feedforward(self, A_prev):
-        self.A = self.activation_f(self.W.dot(A_prev) + self.b)
-        return self.A
+    def forward(self, A_prev):
+        Z = self.W.dot(A_prev) + self.b
+        self.A = self.activation_f(Z)
+        self.d_A = self.d_activation_f(Z)
 
-    def backpropagate(self):
-        pass
+    def backward(self):
+        self.delta = self.next_layer.W.T.dot(self.next_layer.delta) * self.d_A
+        self.d_W = self.delta.dot(self.prev_layer.A.T)
+        self.d_b = np.sum(self.delta, axis=1, keepdims=True)
 
 
-if __name__ == '__main__':
-    input_layer = Input(n_units=64)
-    dense1 = Dense(n_units=15, activation_f = relu)
-    dense1.initialize_parameters(5)
-    x = np.random.randn(5, 1)
-    print(dense1.feedforward(x))
+class Output(Dense):
+
+    def __init__(self, n_units, initializer=HeInitializer(42), activation_f="softmax"):
+        Dense.__init__(self, n_units, initializer, activation_f)
+        self.initial_delta = None
+
+    def forward(self, A_prev):
+        Z = self.W.dot(A_prev) + self.b
+        self.A = self.activation_f(Z)
+
+    def backward(self):
+        self.d_W = self.initial_delta.dot(self.prev_layer.A.T)
+        self.d_b = np.sum(self.initial_delta, axis=1, keepdims=True)
+        self.delta = self.initial_delta
