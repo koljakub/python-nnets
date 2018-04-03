27fca50c2d8488e414b38962d31df9f3c42550fd
Jakub Kol <jakub.kol@hotmail.com>
2018-04-03 13:31:11 +0200

    Reimplement the "losses" module
    Implement the new format of the loss functions.
    Every loss function can now be created by creating
    the object of the LossFunction class. The constructor
    accepts a string name of the function and then dynamically
    selects a callable function from the already defined functions
    in the module. The object itself is a callable object and can
    be directly used to compute the output of the function - eg.:
    
    f = LossFunction("negative_log_loss")
    y_hat = [0, 0.7]
    y = [0, 1]
    result = f(y_hat, y)

--- pynn/losses.py
+++ pynn/losses.py
@@ -1,4 +1,47 @@
 import numpy as np
 
+
+class LossFunction:
+
+    def __init__(self, name):
+        if (name in globals()):
+            self.name = name
+            self.callable_f = get_function(name)
+        else:
+            raise ValueError("Loss function {} not defined!".format(name))
+
+    def __call__(self, y_hat, y):
+        return self.callable_f(y_hat, y)
+
+    def get_initial_delta(self, activation_f):
+        """
+        The function computes initial delta value for the Backpropagation algorithm.
+
+        Let L be some loss function. Let Z be a preactivation of some activation function g, such
+        that A = g(Z). Formally, the initial delta value can be expressed as a partial derivative
+        using the chain rule as follows: dL / dg * dg / dZ.
+
+        The initial delta is abstracted into a lambda function which can be applied to the concrete
+        inputs of the Backpropagation algorithm - the vector (matrix) of predictions and the vector
+        (matrix) of true values.
+        :param loss:
+        :param activation_f:
+        :return:
+        """
+        if (self.name == "negative_log_loss" and (activation_f.name == "softmax" or activation_f.name == "sigmoid")):
+            return lambda y_hat, y: y_hat - y
+
+
+def get_function(func_name):
+    return globals()[func_name]
+
+
 def negative_log_loss(y_hat, y):
     return -np.sum(y * np.log(y_hat + np.finfo(float).eps), axis=0)
+
+if __name__ == '__main__':
+    f = LossFunction("negative_log_loss")
+    y_hat = [0, 0.7]
+    y = [0, 1]
+    result = f(y_hat, y)
+    print(result)
